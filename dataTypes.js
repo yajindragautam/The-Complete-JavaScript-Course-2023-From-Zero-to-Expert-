d08b31e11a5618d77712960e29fa91fe9633d591
yajindra.gautam <yajindra.gautam@ekbana.info>
2023-06-25 17:21:31 +0545

    data types and template literal in js

---
+++ dataTypes.js
@@ -0,0 +1,35 @@
+/*
+Primitive Data Tpes
+1) Number
+2) String
+3) Boolean
+4) Undefined
+5) Null
+4) Symbole
+7) BigInt
+*/
+
+// 1) Number : Floating point numbers -> used for decimal and integer
+let myNumber = 45;
+console.log('Printing Number :', myNumber);
+console.log('Data Type :', typeof myNumber);
+
+// 2 String
+let myName="Yajindra";
+console.log('Printing String :', myName);
+console.log('Data Type :', typeof myName);
+
+// 3) Boolean: true or fals
+let ageAboveTen = true;
+console.log('Printing Boolean :', ageAboveTen);
+console.log('Data Type :', typeof ageAboveTen);
+
+// 4) Undefined : value not assigned
+let undefineMe;
+console.log('Printing Undefined :', undefineMe);
+console.log('Data Type :', typeof undefineMe);
+
+// 5) Null : empty nothng - typeof is object
+let iAmNull = null;
+console.log('Printing Null :', iAmNull);
+console.log('Data Type :', typeof iAmNull);
---
+++ templateLiterals.js
@@ -0,0 +1,17 @@
+let firstName = "Yajindra";
+let lastName = "Gautam";
+const job = "NodeJS Deeveloper";
+
+// Without using template literals
+console.log("Hi my name is "+firstName+ lastName + " and I am a "+ job);
+
+// Using Template Literal
+console.log(`Hi my name is ${firstName} ${lastName} and I am a ${job}`);
+
+// Break line using string
+console.log('String with \n\ multiple \n\ lines .');
+
+// Break Line using template literal
+console.log(`String with 
+ multiple 
+ lines .`);
